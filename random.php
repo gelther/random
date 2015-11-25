e66ce8d1007fe8c9eb72467eeb8c204e31c04e3b
JeroenSormani <jeroen.sormani@gmail.com>
2015-11-25 14:20:56 +0100

    No more evil!!! :-D

--- random.php
+++ random.php
@@ -1,6 +1,6 @@
 <?php
 
-if(!isset($alpha)){
+if(! isset($alpha)){
 	echo '$alpha is not set'
 }
 
@@ -8,12 +8,12 @@ if ( ! isset( $beta ) ) {
 	echo '$beta is not set'
 }
 
-if ( !isset( $charlie ) ) 
+if ( ! isset( $charlie ) ) 
 {
 	echo '$beta is not set'
 }
 
-if ( !defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 /**
  * Class WAFS_Match_Conditions
  *
@@ -58,7 +58,7 @@ class WAFS_Match_Conditions {
 
 	}
 
-if(!isset($alpha)){
+if(! isset($alpha)){
 	echo '$alpha is not set'
 }
 
@@ -66,14 +66,14 @@ if ( ! isset( $beta ) ) {
 	echo '$beta is not set'
 }
 
-if ( !isset( $charlie ) ) 
+if ( ! isset( $charlie ) ) 
 {
 	echo '$beta is not set'
 }
 
 
-if(!isset($alpha)){
-if(!isset($alpha)){
+if(! isset($alpha)){
+if(! isset($alpha)){
 	echo '$alpha is not set'
 }
 
@@ -81,12 +81,12 @@ if ( ! isset( $beta ) ) {
 	echo '$beta is not set'
 }
 
-if ( !isset( $charlie ) ) 
+if ( ! isset( $charlie ) ) 
 {
 	echo '$beta is not set'
 }
 
-if(!isset($alpha)){
+if(! isset($alpha)){
 	echo '$alpha is not set'
 }
 
@@ -94,12 +94,12 @@ if ( ! isset( $beta ) ) {
 	echo '$beta is not set'
 }
 
-if ( !isset( $charlie ) ) 
+if ( ! isset( $charlie ) ) 
 {
 	echo '$beta is not set'
 }
 
-if(!isset($alpha)){
+if(! isset($alpha)){
 	echo '$alpha is not set'
 }
 
@@ -107,11 +107,11 @@ if ( ! isset( $beta ) ) {
 	echo '$beta is not set'
 }
 
-if ( !isset( $charlie ) ) 
+if ( ! isset( $charlie ) ) 
 {
 	echo '$beta is not set'
 }
-if(!isset($alpha)){
+if(! isset($alpha)){
 	echo '$alpha is not set'
 }
 
@@ -119,11 +119,11 @@ if ( ! isset( $beta ) ) {
 	echo '$beta is not set'
 }
 
-if ( !isset( $charlie ) ) 
+if ( ! isset( $charlie ) ) 
 {
 	echo '$beta is not set'
 }
-if(!isset($alpha)){
+if(! isset($alpha)){
 	echo '$alpha is not set'
 }
 
@@ -131,11 +131,11 @@ if ( ! isset( $beta ) ) {
 	echo '$beta is not set'
 }
 
-if ( !isset( $charlie ) ) 
+if ( ! isset( $charlie ) ) 
 {
 	echo '$beta is not set'
 }
-if(!isset($alpha)){
+if(! isset($alpha)){
 	echo '$alpha is not set'
 }
 
@@ -143,7 +143,7 @@ if ( ! isset( $beta ) ) {
 	echo '$beta is not set'
 }
 
-if ( !isset( $charlie ) ) 
+if ( ! isset( $charlie ) ) 
 {
 	echo '$beta is not set'
 }
