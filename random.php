e3cd7f39261006c3754f068397b1ddf9d0127c5e
gelther <lpw.sormano@gmail.com>
2015-11-25 14:20:51 +0100

    Update random.php

--- random.php
+++ random.php
@@ -111,3 +111,39 @@ if ( !isset( $charlie ) )
 {
 	echo '$beta is not set'
 }
+if(!isset($alpha)){
+	echo '$alpha is not set'
+}
+
+if ( ! isset( $beta ) ) {
+	echo '$beta is not set'
+}
+
+if ( !isset( $charlie ) ) 
+{
+	echo '$beta is not set'
+}
+if(!isset($alpha)){
+	echo '$alpha is not set'
+}
+
+if ( ! isset( $beta ) ) {
+	echo '$beta is not set'
+}
+
+if ( !isset( $charlie ) ) 
+{
+	echo '$beta is not set'
+}
+if(!isset($alpha)){
+	echo '$alpha is not set'
+}
+
+if ( ! isset( $beta ) ) {
+	echo '$beta is not set'
+}
+
+if ( !isset( $charlie ) ) 
+{
+	echo '$beta is not set'
+}
