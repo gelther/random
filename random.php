713e5ca92455fd0342d455ea37b90e025ac40517
gelther <lpw.sormano@gmail.com>
2015-11-25 13:35:52 +0100

    Update random.php

--- random.php
+++ random.php
@@ -13,3 +13,16 @@ if ( !isset( $charlie ) )
 	echo '$beta is not set'
 }
 
+if ( !defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+/**
+ * Class WAFS_Match_Conditions
+ *
+ *The WAFS Match Conditions class handles the matching rules for Free Shipping
+ *
+ * @class 		WAFS_Match_Conditions
+ * @author		Jeroen Sormani
+ * @package		WooCommerce Advanced Free Shipping
+ * @version		1.0.0
+ */
+class WAFS_Match_Conditions {
+
